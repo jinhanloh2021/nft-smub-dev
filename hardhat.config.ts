7c7f33cef14e5d3efb4cb0ccc95ea9b9635b2bc5
jinhanloh2021 <jinhan.loh.2021@scis.smu.edu.sg>
2023-06-15 17:25:54 +0800

    Initialise NFT project.

---
+++ hardhat.config.ts
@@ -0,0 +1,50 @@
+import { HardhatUserConfig } from 'hardhat/config';
+import '@nomicfoundation/hardhat-toolbox';
+import 'dotenv/config';
+import '@nomiclabs/hardhat-etherscan';
+import 'hardhat-deploy';
+
+const config: HardhatUserConfig = {
+  solidity: {
+    compilers: [{ version: '0.8.7' }, { version: '0.6.12' }],
+  },
+  defaultNetwork: 'hardhat',
+  networks: {
+    hardhat: {
+      chainId: 31337,
+    },
+    sepolia: {
+      url: process.env.SEPOLIA_RPC_URL || '',
+      accounts: [process.env.SEPOLIA_PRIVATE_KEY || '0xkey'],
+      chainId: 11155111,
+    },
+    localhost: {
+      url: 'http://127.0.0.1:8545/',
+      // accounts: [provided by hardhat]
+      chainId: 31337,
+    },
+  },
+  etherscan: {
+    apiKey: process.env.ETHERSCAN_API_KEY || 'Key',
+  },
+  gasReporter: {
+    enabled: false,
+    currency: 'USD',
+    outputFile: 'gas-report.txt',
+    noColors: true,
+    // coinmarketcap: process.env.COINMARKETCAP_API_KEY,
+  },
+  namedAccounts: {
+    deployer: {
+      default: 0,
+    },
+    player: {
+      default: 1,
+    },
+  },
+  mocha: {
+    timeout: 300000,
+  },
+};
+
+export default config;
